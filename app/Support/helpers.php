2e8a68edac4844af3d6e94be4dc9a5d573d3dfa5
jiahaiyong <jiahaiyong@qimai.net>
2019-03-01 14:41:53 +0800

    辅助函数添加

---
+++ app/Support/helpers.php
@@ -0,0 +1,20 @@
+<?php  
+if (! function_exists('testt')) {
+    /**
+     *
+     * 测试公共函数
+     *
+     * @return string
+     */
+    function testt()
+    {
+        return 'wechatapp_test';
+    }
+}
+
+
+?>
+
+
+
+
