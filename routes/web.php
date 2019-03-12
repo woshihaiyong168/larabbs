e5e285a9b2aeabe742e7f7724601c31c84574ec3
jiahaiyong <jiahaiyong@qimai.net>
2019-03-12 14:52:51 +0800

    个人页面

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,9 @@
 
 Route::get('/', 'PagesController@root')->name('root');
 Auth::routes();
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+//Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+//Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+//Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
 
 //Route::get('/home', 'HomeController@index')->name('home');
